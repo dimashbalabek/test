1b416e135d070a7322def52e4bd85935183aa23e
dimashbalabek <144226438+dimashbalabek@users.noreply.github.com>
2024-05-21 22:51:49 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_demo/pages/home.dart';
+import 'package:flutter_application_demo/pages/search.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SeacrhPage(),
+    );
+  }
+}
